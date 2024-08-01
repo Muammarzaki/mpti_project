91d49ab8fab81b7b0c4588e296b632171637693c
muammarzaki <muammarzaki2004@gmail.com>
2024-08-01 12:40:06 +0700

    push ajah

---
+++ auth_resource_server/src/main/java/uin/AuthResourceServerApplication.java
@@ -0,0 +1,13 @@
+package uin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthResourceServerApplication.class, args);
+	}
+
+}
---
+++ auth_resource_server/src/test/java/uin/AuthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
